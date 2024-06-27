26960f7e1fde96ebca1a2cd7a6dc6fb9323b7b34
Melvin jerry <+>
2024-06-27 12:34:56 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+const mondayWork= function(activity= 'go to the office'){ 
+   return`This Monday, I will ${activity}.`
+}
+mondayWork('work from home')
+
+function saturdayFun(activity = "roller-skate"){
+return `This Saturday, I want to ${activity}!`;
+}
+
+
+saturdayFun('bathe my dog')
+
+function wrapAdjective(string="*"){
+    function string(brace='special') {
+        return function(){
+            `You are ${brace}`
+        }
+        
+    }
+}
+wrapAdjective("*")()
